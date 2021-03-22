b4f88ec90bb58a31f4036b0f6a200342191c487b
liangjq <420030650@qq.com>
2021-03-22 23:40:39 +0800

    首次提交，配置好大版本（SpringCloud、SpringCloud alibaba）

---
+++ README.md
@@ -0,0 +1,40 @@
+# Mix
+
+#### 介绍
+一个开源的基于SpringCloud + Vue 的前后端分离权限管理系统。
+
+Mix(混合)，因为会在学习过程中不断地在这个项目中实践所学，所以与工作项目相比，所选技术不一定是最优解，同学们可以看作是一个技术大DEMO。
+
+我是希望能将所学都体现在这个开源项目中，希望同学们能在这里找到想学的技术的落地实践，当然如果没找到可以给我提下 :)
+
+#### 技术选型
+##### 后端
+技术 | 作用 | 版本
+----|----|---
+JDK | Java 开发套件 | 1.8+
+Spring Framework | 容器 |5.3.5
+Spring Boot | spring 快速开发框架|2.4.4
+Spring Cloud | 微服务全家桶 |2020.0.2
+Spring Cloud Alibaba | 阿里微服务套件 |2020.0.RC1
+MyBatis | ORM框架
+MyBatis Generator | 代码生成器
+PageHelper | MyBatis物理分页插件
+Druid | 数据库连接池
+Redis | 分布式缓存数据库
+Swagger2 | 接口文档工具
+jenkins | 持续集成工具
+docker | 容器工具
+maven | 项目构建管理
+
+
+未完待续
+  
+
+##### 前端
+技术 | 作用 
+----|----|
+vue | js框架 
+element-ui | vue 框架 
+vue-element-admin | 后台管理脚手架 
+
+
---
+++ mix_admin/src/main/java/cn/liangjq/mix/admin/AdminApplication.java
@@ -0,0 +1,28 @@
+package cn.liangjq.mix.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+/**
+ * @author ： liangjq
+ * @description ： admin入口
+ * @date ： 2021/3/22
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    /**
+     * 设置时区
+     */
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+}
