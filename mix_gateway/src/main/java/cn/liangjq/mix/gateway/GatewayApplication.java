f1c38630e01f5a497e26b5f1defac177496243ad
liangjq <420030650@qq.com>
2021-03-30 23:27:48 +0800

    增加网关模块

---
+++ mix_gateway/src/main/java/cn/liangjq/mix/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.liangjq.mix.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ： liangjianqiang
+ * @description ： 网关启动类
+ * @date ： 2021/3/30
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
