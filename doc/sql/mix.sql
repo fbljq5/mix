033e425d0747e79b98278f1c4fdecc11e6d64675
liangjq <420030650@qq.com>
2021-04-22 15:28:32 +0800

    补充最新的数据库脚本和nacos数据库脚本

--- doc/sql/mix.sql
+++ doc/sql/mix.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 23/03/2021 23:41:32
+ Date: 22/04/2021 15:27:37
 */
 
 SET NAMES utf8mb4;
@@ -22,82 +22,107 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_menu`;
 CREATE TABLE `t_menu`  (
-  `id` bigint(0) NOT NULL COMMENT '菜单ID',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '菜单ID',
   `menu_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
-  `parent_id` bigint(0) NULL DEFAULT NULL COMMENT '父菜单ID',
-  `order_sort` int(0) NULL DEFAULT NULL COMMENT '显示顺序',
+  `parent_id` bigint NULL DEFAULT NULL COMMENT '父菜单ID',
+  `order_sort` int NULL DEFAULT NULL COMMENT '显示顺序',
   `path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '路由地址',
   `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '组件路径',
-  `visible` tinyint(0) NULL DEFAULT NULL COMMENT '菜单显示状态（0显示 1隐藏）',
-  `status` tinyint(0) NULL DEFAULT NULL COMMENT '菜单状态（0正常 1停用）',
+  `visible` tinyint NULL DEFAULT NULL COMMENT '菜单显示状态（0显示 1隐藏）',
+  `status` tinyint NULL DEFAULT NULL COMMENT '菜单状态（0正常 1停用）',
   `perms` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单权限标识',
   `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `gmt_modified` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `gmt_create` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_menu
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for t_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role`;
 CREATE TABLE `t_role`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '角色ID',
   `role_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色名称',
   `role_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色编码',
-  `role_sort` int(0) NULL DEFAULT NULL COMMENT '显示顺序',
-  `status` tinyint(0) NULL DEFAULT NULL COMMENT '角色状态（0正常 1停用）',
-  `is_delete` tinyint(0) NULL DEFAULT NULL COMMENT '是否删除（0代表存在 1代表删除）',
+  `role_sort` int NULL DEFAULT NULL COMMENT '显示顺序',
+  `status` tinyint NULL DEFAULT NULL COMMENT '角色状态（0正常 1停用）',
+  `is_delete` tinyint NULL DEFAULT NULL COMMENT '是否删除（0代表存在 1代表删除）',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `gmt_modified` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `gmt_create` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES (1, 'ADMIN', 'ADMIN', 1, 1, 0, '管理员', '2021-03-25 11:41:23', '2021-03-26 11:41:25');
+INSERT INTO `t_role` VALUES (2, 'USER', 'USER', 2, 1, 0, '普通用户', '2021-03-25 11:41:43', '2021-03-25 11:41:46');
 
 -- ----------------------------
 -- Table structure for t_role_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role_menu`;
 CREATE TABLE `t_role_menu`  (
-  `id` bigint(0) NOT NULL COMMENT '主键ID',
-  `role_id` bigint(0) NOT NULL COMMENT '角色ID',
-  `menu_id` bigint(0) NOT NULL COMMENT '菜单ID',
-  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `gmt_modified` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `role_id` bigint NOT NULL COMMENT '角色ID',
+  `menu_id` bigint NOT NULL COMMENT '菜单ID',
+  `gmt_create` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role_menu
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名称',
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电话号码',
   `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
-  `status` tinyint(0) NULL DEFAULT NULL COMMENT '帐号状态（0正常 1停用）',
-  `is_delete` tinyint(0) NULL DEFAULT NULL COMMENT '是否已删除（0正常 1已删除）',
-  `login_date` datetime(0) NULL DEFAULT NULL COMMENT '最后登录时间',
+  `status` tinyint NULL DEFAULT NULL COMMENT '帐号状态（0正常 1停用）',
+  `is_delete` tinyint NULL DEFAULT NULL COMMENT '是否已删除（0正常 1已删除）',
+  `login_date` datetime NULL DEFAULT NULL COMMENT '最后登录时间',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `gmt_modified` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `gmt_create` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (1, '123', '4', '32', '12345', 24, 0, '2021-03-24 16:11:52', '234', '2021-03-24 16:11:45', '2021-03-24 16:11:47');
 
 -- ----------------------------
 -- Table structure for t_user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_role`;
 CREATE TABLE `t_user_role`  (
-  `id` bigint(0) NOT NULL COMMENT '主键ID',
-  `user_id` bigint(0) NOT NULL COMMENT '用户ID',
-  `role_id` int(0) NOT NULL COMMENT '角色ID',
-  `gmt_create` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `gmt_modified` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `role_id` bigint NOT NULL COMMENT '角色ID',
+  `gmt_create` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_role
+-- ----------------------------
+INSERT INTO `t_user_role` VALUES (1, 1, 1, '2021-03-25 11:42:01', '2021-03-26 11:42:03');
+INSERT INTO `t_user_role` VALUES (2, 1, 2, '2021-04-22 14:39:33', '2021-04-22 14:39:37');
 
 SET FOREIGN_KEY_CHECKS = 1;
