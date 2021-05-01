f6c7e4ddcc61ba42e62f5e787a60f7c5cf563e4e
liangjq <420030650@qq.com>
2021-05-01 08:56:05 +0800

    美化登录页面

--- mix-vue/src/views/Login.vue
+++ mix-vue/src/views/Login.vue
@@ -1,100 +1,90 @@
 <template>
-  <div class="login-wrap">
-    <div class="form-wrap">
-      <div class="sys-title">后台管理系统</div>
-      <a-form
-        :model="formState"
-        @finish="handleFinish"
-        @finishFailed="handleFinishFailed"
-        class="loginForm"
-      >
-        <a-form-item>
-          <a-input v-model:value="formState.user" placeholder="Username">
-            <template #prefix
-              ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
-            /></template>
-          </a-input>
-        </a-form-item>
-        <a-form-item>
-          <a-input
-            v-model:value="formState.password"
-            type="password"
-            placeholder="Password"
-          >
-            <template #prefix
-              ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
-            /></template>
-          </a-input>
-        </a-form-item>
-        <a-form-item>
-          <a-button
-            type="primary"
-            html-type="submit"
-            :disabled="formState.user === '' || formState.password === ''"
-          >
-            登录
-          </a-button>
-        </a-form-item>
-      </a-form>
+  <div class="login-box">
+    <div class="login-logo">
+      <svg-icon icon-class="logo" />
+      <h1>MIX</h1>
     </div>
+    <a-form
+      layout="horizontal"
+      :model="formInline"
+      @submit.prevent="handleSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-model:value="formInline.username"
+          size="large"
+          placeholder="admin"
+        >
+          <template #prefix><user-outlined type="user" /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-model:value="formInline.password"
+          size="large"
+          type="password"
+          placeholder="123456"
+          autocomplete="new-password"
+        >
+          <template #prefix><lock-outlined type="user" /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button
+          type="primary"
+          html-type="submit"
+          size="large"
+          :loading="loading"
+          block
+        >
+          登录
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 <script lang="ts">
-import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
-import { ValidateErrorEntity } from "ant-design-vue/es/form/interface";
-import { defineComponent, reactive, UnwrapRef } from "vue";
+import { defineComponent, reactive, toRef, toRefs, UnwrapRef } from "vue";
 interface FormState {
   user: string;
   password: string;
 }
 export default defineComponent({
+  name: "Login",
+
   setup() {
-    const formState: UnwrapRef<FormState> = reactive({
-      user: "",
-      password: "",
+    const stat = reactive({
+      loading: false,
+      formInline: {
+        username: "",
+        password: "",
+      },
     });
-    const handleFinish = (values: FormState) => {
-      console.log(values, formState);
-    };
-    const handleFinishFailed = (errors: ValidateErrorEntity<FormState>) => {
-      console.log(errors);
-    };
+
     return {
-      formState,
-      handleFinish,
-      handleFinishFailed,
+      ...toRefs(stat),
     };
-  },
-  components: {
-    UserOutlined,
-    LockOutlined,
   },
 });
 </script>
 <style lang="less" scoped>
-.login-wrap {
+.login-box {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  padding-top: 240px;
+  flex-direction: column;
+  align-items: center;
   background: url("~@/assets/images/login-bg.jpg");
-  // background: green;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  .sys-title {
-    width: 100%;
-    line-height: 50px;
-    text-align: center;
-    font-size: 20px;
-    color: #fff;
-    border-bottom: 1px solid #ddd;
+  background-size: 100%;
+
+  img {
+    height: 48px;
   }
-  .form-wrap {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    width: 350px;
-    margin: -190px 0 0 -175px;
-    border-radius: 5px;
-    background: rgba(255, 255, 255, 0.3);
-    overflow: hidden;
+
+  h1 {
+    margin-left: 10px;
+    margin-bottom: 0;
   }
 }
 </style>
