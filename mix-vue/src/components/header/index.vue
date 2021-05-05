b484e95de45ee17116ee93d0c2bfa4f82078eb31
liangjq <420030650@qq.com>
2021-05-05 19:57:57 +0800

    添加LOGO

--- mix-vue/src/components/header/index.vue
+++ mix-vue/src/components/header/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <a-layout-header style="background: #fff; padding: 0">
-    <a-button>TESTa</a-button>
-  </a-layout-header>
+  <div class="right-options">
+    <a-layout-header style="background: #fff; padding: 0">
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a @click.prevent="doLogout" class="logoutBtn"
+          ><poweroff-outlined /> 退出登录</a
+        >
+      </a-menu>
+    </a-layout-header>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
@@ -10,3 +21,9 @@ export default defineComponent({
   name: "MixHeader",
 });
 </script>
+<style lang="less" scoped>
+.logoutBtn {
+  align-items: center;
+  margin-left: 93%;
+}
+</style>
--- mix-vue/src/components/logo/index.vue
+++ mix-vue/src/components/logo/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="logo" />
+  <div class="logo">
+    <img src="~@/assets/images/logo.png" style="width: 100%" />
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
