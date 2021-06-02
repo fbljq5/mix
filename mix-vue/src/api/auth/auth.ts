65495b43165a84ac0f93a7c169e5fb6f3581038a
liangjq <420030650@qq.com>
2021-06-02 23:09:35 +0800

    从mix项目中移除前端项目，前端项目将作为独立项目（mix-vue），地址为：  https://github.com/fbljq5/mix-vue.git ; https://gitee.com/RoyLiang/mix-vue.git

--- mix-vue/src/api/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from '@/utils/request'
-
-export function login(parameter: any) {
-    return Request.axiosInstance({
-        url: '/mix-auth/auth/login',
-        method: 'post',
-        data: parameter
-    })
-}
